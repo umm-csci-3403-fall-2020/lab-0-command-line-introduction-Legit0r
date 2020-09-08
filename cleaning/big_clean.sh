6ea8fa0d7c44b0b30f0519c6a1ab3f4ff4d03175
Legit0r <michaelfairbanks5469@gmail.com>
2020-09-08 05:47:45 -0500

    Add big_clean.sh; 3 Fails

---
+++ cleaning/big_clean.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+
+#tar file being cleaned
+tar=$1
+#Starting Directory
+cleaning=$(pwd)
+#Temp Directory
+SCRATCH=`mktemp --directory`
+#Extracts 'tar' into temp directory
+tar -zxf "$tar" -C "$SCRATCH"
+#Enters temp directory
+cd "$SCRATCH" || exit
+#Cleans
+grep -rq 'DELETE ME!' . | xargs rm
+#Takes filename without .tgz
+tar_name=$(basename -s .tgz "$tar")
+#Creates new tar file
+tar -czf cleaned_"$tar" "$tar_name"
+#Moves cleaned file into Starting Directory
+mv cleaned_"$tar" "$cleaning"
